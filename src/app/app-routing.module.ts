216de332277125c7767bfe99bcf571cac8a2a03d
Timothy Mutegi <mutegi.timothy538@gmail.com>
2020-02-19 17:52:31 +0300

    added routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { StepTwoComponent } from './components/step-two/step-two.component';
+import { StepThreeComponent } from './components/step-three/step-three.component';
+import { StepOneComponent } from './components/step-one/step-one.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: StepOneComponent},
+  { path: 'step-two', component: StepTwoComponent},
+  { path: 'step-three', component: StepThreeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { StepOneComponent } from './components/step-one/step-one.component';
+import { StepTwoComponent } from './components/step-two/step-two.component';
+import { StepThreeComponent } from './components/step-three/step-three.component';
+import { StoreModule } from '@ngrx/store';
+import { reducer } from './reducers/step-one-reducer';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    StepOneComponent,
+    StepTwoComponent,
+    StepThreeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    StoreModule.forRoot({
+      user: reducer
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
