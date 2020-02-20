9ba7a45f2ee769020bb84f8856e6142720aa06ac
Timothy Mutegi <mutegi.timothy538@gmail.com>
2020-02-20 08:48:50 +0300

    Create Api service

---
+++ src/app/services/api.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+  baseUrl = 'https://obscure-beyond-81246.herokuapp.com/api';
+
+  constructor(private http: HttpClient) { }
+
+  postData(endpoint: string, data: JSON) {
+    return this.http.post<any>(this.baseUrl + endpoint, data);
+  }
+}
