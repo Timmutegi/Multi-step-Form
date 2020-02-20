15ae5323235e5398684b9209bc3ec215b7e55d49
Timothy Mutegi <mutegi.timothy538@gmail.com>
2020-02-20 08:50:03 +0300

    created summary route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { StepTwoComponent } from './components/step-two/step-two.component';
 import { StepThreeComponent } from './components/step-three/step-three.component';
 import { StepOneComponent } from './components/step-one/step-one.component';
+import { SummaryComponent } from './components/summary/summary.component';
 
 
 const routes: Routes = [
-  { path: '', component: StepOneComponent},
-  { path: 'step-two', component: StepTwoComponent},
-  { path: 'step-three', component: StepThreeComponent}
+  { path: '', component: StepOneComponent },
+  { path: 'step-two', component: StepTwoComponent },
+  { path: 'step-three', component: StepThreeComponent },
+  { path: 'summary', component: SummaryComponent }
 ];
 
 @NgModule({
